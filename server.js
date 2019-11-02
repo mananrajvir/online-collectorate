a8f8159da1364479a8104a60e6899f8076a7043b
Namit Nathwani <namsnath@gmail.com>
2019-11-02 15:36:44 +0530

    Adds routes to populate data

--- server.js
+++ server.js
@@ -8,10 +8,22 @@ const API_PORT = 3001;
 
 // Data JSONs
 const citizensJSON = require('./data/Citizens.json');
-
+const complaintsJSON = require('./data/Complaints.json');
+const duesJSON = require('./data/Dues.json');
+const landRecordsJSON = require('./data/LandRecords.json');
+const landSalesJSON = require('./data/LandSales.json');
+const publicUtilitiesJSON = require('./data/PublicUtilities.json');
+const utilitiesListJSON = require('./data/UtilitiesList.json');
 
 // Models
 const Citizen = require('./models/Citizen');
+const Complaint = require('./models/Complaint');
+const Due = require('./models/Due');
+const LandRecord = require('./models/LandRecord');
+const LandSale = require('./models/LandSale');
+const PublicUtility = require('./models/PublicUtility');
+const Utility = require('./models/Utility');
+
 
 mongoose.connect("mongodb://localhost:27017/collectorate", {
 	useFindAndModify: false,
@@ -74,12 +86,96 @@ function doLandRecordUpdate(query, update) {
 	});
 }
 
+function doLandSaleUpdate(query, update) {
+	return new Promise((resolve, reject) => {
+		LandSale.findOneAndUpdate(query, update,
+			{ upsert: true, new: true, setDefaultsOnInsert: true },
+			function (err, doc) {
+				if (err) return reject(err);
+				return resolve(doc);
+			});
+	});
+}
+
+function doPublicUtilityUpdate(query, update) {
+	return new Promise((resolve, reject) => {
+		PublicUtility.findOneAndUpdate(query, update,
+			{ upsert: true, new: true, setDefaultsOnInsert: true },
+			function (err, doc) {
+				if (err) return reject(err);
+				return resolve(doc);
+			});
+	});
+}
+
+
+function doUtilityUpdate(query, update) {
+	return new Promise((resolve, reject) => {
+		Utility.findOneAndUpdate(query, update,
+			{ upsert: true, new: true, setDefaultsOnInsert: true },
+			function (err, doc) {
+				if (err) return reject(err);
+				return resolve(doc);
+			});
+	});
+}
+
+
+
 app.get('/parseCitizens', (req, res) => {
 	var promises = citizensJSON.map((v) => doCitizenUpdate({aadhar: v.aadhar}, v));
 
 	Promise.all(promises)
 	.then(result => res.json(result))
 	.catch(err => logger.error(err));
+});
+
+app.get('/parseComplaints', (req, res) => {
+	var promises = complaintsJSON.map((v) => doComplaintUpdate({ aadhar: v.aadhar }, v));
+
+	Promise.all(promises)
+		.then(result => res.json(result))
+		.catch(err => logger.error(err));
+});
+
+app.get('/parseDues', (req, res) => {
+	var promises = duesJSON.map((v) => doDueUpdate({ aadhar: v.aadhar }, v));
+
+	Promise.all(promises)
+		.then(result => res.json(result))
+		.catch(err => logger.error(err));
+});
+
+app.get('/parseLandRecords', (req, res) => {
+	var promises = landRecordsJSON.map((v) => doLandRecordUpdate({ aadhar: v.aadhar }, v));
+
+	Promise.all(promises)
+		.then(result => res.json(result))
+		.catch(err => logger.error(err));
+});
+
+app.get('/parseLandSales', (req, res) => {
+	var promises = landSalesJSON.map((v) => doLandSaleUpdate(v, v));
+
+	Promise.all(promises)
+		.then(result => res.json(result))
+		.catch(err => logger.error(err));
+});
+
+app.get('/parsePublicUtilities', (req, res) => {
+	var promises = publicUtilitiesJSON.map((v) => doPublicUtilityUpdate({ aadhar: v.aadhar }, v));
+
+	Promise.all(promises)
+		.then(result => res.json(result))
+		.catch(err => logger.error(err));
+});
+
+app.get('/parseUtilities', (req, res) => {
+	var promises = utilitiesListJSON.map((v) => doUtilityUpdate(v, v));
+
+	Promise.all(promises)
+		.then(result => res.json(result))
+		.catch(err => logger.error(err));
 });
 
 app.listen(API_PORT, () => logger.info(`Listening on port ${API_PORT}`));
