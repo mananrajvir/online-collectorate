19f19c8e0e096d8a791890b8ad7c3532b2224e82
Namit Nathwani <namsnath@gmail.com>
2019-11-02 14:44:07 +0530

    feat: adds due

---
+++ models/Due.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+var dueSchema = mongoose.Schema({	// From the data given in the sheet
+	due_id: {
+		type: Number,
+		required: true,
+		unique: true,
+	},
+	aadhar_number: String,
+	due_amount: Number,
+	pay_to_office: String,
+	due_type: String,
+	payment: {
+		payment_id: Number,
+		date: Date,
+		amount: Number,
+		payment_method: String
+	}
+});
+
+module.exports = mongoose.model("Due", dueSchema);
